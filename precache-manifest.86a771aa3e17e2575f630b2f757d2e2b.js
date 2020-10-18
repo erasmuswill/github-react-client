d7d5c40ba712bcd81c4a6d53eba125f808265c9d
Wilhelm Erasmus <iam@erasmuswill.dev>
2020-10-18 02:10:46 +0200

    Updates

---
+++ precache-manifest.86a771aa3e17e2575f630b2f757d2e2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9160d2b62adff6c551e94fa1ffe8788a",
+    "url": "/github-react-client/index.html"
+  },
+  {
+    "revision": "9ba86d1c80ccc7842c87",
+    "url": "/github-react-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "688f0b3338c2593c700d",
+    "url": "/github-react-client/static/js/2.7b78e425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-react-client/static/js/2.7b78e425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba86d1c80ccc7842c87",
+    "url": "/github-react-client/static/js/main.a086eb81.chunk.js"
+  },
+  {
+    "revision": "aa1b18ce082f6aecc8c6",
+    "url": "/github-react-client/static/js/runtime-main.cb9cc738.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-react-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
