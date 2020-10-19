d1d5e98a3808807c1a227ab6b18cb2e0c21c8c88
Wilhelm Erasmus <iam@erasmuswill.dev>
2020-10-20 01:10:11 +0200

    chore(axios): adds boilerplate for API token addition

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 import axiosRetry from 'axios-retry';
 import { makeUseAxios } from 'axios-hooks'
 
-const instance = axios.create({ baseURL: "https://api.github.com" });
+const instance = axios.create({
+  baseURL: "https://api.github.com",
+  // Add your username and access token here:
+  // auth: {
+  //   username: "erasmuswill",
+  //   password: "xxx",
+  // },
+});
 
 axiosRetry(instance, { retryDelay: axiosRetry.exponentialDelay, retries: 5 });
 
