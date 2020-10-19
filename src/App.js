7a2681caa06420cde69ac003a0fefd42a044945b
Wilhelm Erasmus <iam@erasmuswill.dev>
2020-10-19 18:45:42 +0200

    chore: cleans up index

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import { useState } from "react";
 import "./App.css";
-import { Router } from "@reach/router";
+import { Redirect, Router } from "@reach/router";
 import ActivityLog from "./ActivityLog";
 
 function App() {
-  const [type, setType] = useState("");
-  const [resource, setResource] = useState("");
-  const [query, setQuery] = useState("");
   return (
     <div>
       Header with controls for input of ActivityLog variables
       <Router basepath="/github-react-client">
         <ActivityLog path="/:type/:resource/:query" />
         <ActivityLog path="/:type/:resource/" />
+        <Redirect to="/user/erasmuswill" default  />
       </Router>
     </div>
   );
