19b6619f9d5cb3fa90287464fe53b6dad309bd09
Wilhelm Erasmus <iam@erasmuswill.dev>
2020-10-19 16:08:54 +0200

    feat(ActivityLog): adds activity fetching

--- src/ActivityLog/index.js
+++ src/ActivityLog/index.js
@@ -1,1 +1,39 @@
-export default () => <>Hi</>;
+import { useEffect, useState } from "react";
+import axios from "../axios";
+
+export default ({ type, resource }) => {
+  const [data, setData] = useState([]);
+  const [etag, setEtag] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [interval, setIntervalRef] = useState(null);
+  const fetchData = () => {
+    if (interval) clearInterval(interval);
+    let config = { url: "/" + type + "s/" + resource + "/events" };
+    if (etag) config = { ...config, headers: { "If-None-Match": etag } };
+    axios(config)
+      .then(
+        ({
+          data: newData,
+          headers: { etag, "x-poll-interval": poll = 60 },
+        }) => {
+          console.log({ newData, poll, etag });
+          setData([...newData, ...data]);
+          setEtag(etag);
+
+          setIntervalRef(setInterval(() => fetchData(), poll * 1000));
+          setLoading(false);
+        }
+      )
+      .catch((error) => {
+        if (error.response && error.response.status === 304)
+          console.log("Not modified!");
+        else alert("Please refresh the page");
+      });
+  };
+  useEffect(() => {
+    if (interval) clearInterval(interval);
+    fetchData();
+  }, []);
+  console.log({ type, resource });
+  return <div>Hi</div>;
+};
