105b3ead09655b6b5ec4e8f8a2f224c19418dea0
Wilhelm Erasmus <iam@erasmuswill.dev>
2020-10-20 01:06:03 +0200

    feat(ActivityLog): adds filtering

--- src/ActivityLog/index.js
+++ src/ActivityLog/index.js
@@ -1,14 +1,31 @@
-import { Avatar, Tabs, Tab, Box, Heading, DataTable, Text } from "grommet";
+import { Avatar, Tabs, Tab, Box, Heading, Text, TextInput } from "grommet";
 import Overview from "./Overview";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "../axios";
 import BlockUi from "react-block-ui";
+import Fuse from "fuse.js";
 
 const ActivityLog = ({ type, resource }) => {
   const [data, setData] = useState([]);
   const [etag, setEtag] = useState("");
   const [loading, setLoading] = useState(true);
   const [interval, setIntervalRef] = useState(null);
+  const [filter, setFilter] = useState("");
+  const fuse = useMemo(
+    () =>
+      data.length
+        ? new Fuse(data, {
+            keys: ["id", "type", "repo.name"],
+          })
+        : { search: () => [] },
+    [data]
+  );
+  const processedData = useMemo(() => {
+    if (!filter) return data;
+    console.log(fuse.search(filter));
+    // return data;
+    return fuse.search(filter).map(({ item }) => item);
+  }, [data, fuse, filter]);
   const fetchData = () => {
     if (interval) clearInterval(interval);
     let config = { url: "/" + type + "s/" + resource + "/events" };
@@ -42,6 +59,15 @@ const ActivityLog = ({ type, resource }) => {
   return (
     <Box pad="medium" basis="medium">
       <BlockUi loading={loading}>
+        <Box fill flex justify="center" align="center">
+          <Box pad="medium">
+            <TextInput
+              placeholder="search"
+              value={filter}
+              onChange={({ target: { value } }) => setFilter(value)}
+            />
+          </Box>
+        </Box>
         {data.length ? (
           <>
             <Box direction="row" justify="center" alignItems="center" flex>
@@ -52,11 +78,11 @@ const ActivityLog = ({ type, resource }) => {
               ></Avatar>
               <Heading margin="small">{data[0].actor.display_login}</Heading>
             </Box>
-        <Tabs>
-          <Tab title="Top Repos">
-            <Overview data={data} />
-          </Tab>
-        </Tabs>
+            <Tabs>
+              <Tab title="Top Repos">
+                <Overview data={processedData} />
+              </Tab>
+            </Tabs>
           </>
         ) : (
           <Text>No content (yet)</Text>
