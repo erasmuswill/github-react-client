a53a6874e8709cda8ae51fe11e2d7bd8ca1c8c67
Wilhelm Erasmus <iam@erasmuswill.dev>
2020-10-22 02:02:02 +0200

    refactor: refactors hook methods to piping

--- src/ActivityLog/Overview.js
+++ src/ActivityLog/Overview.js
@@ -2,29 +2,40 @@ import { Box, List } from "grommet";
 import { compose, withMemo } from "@truefit/bach";
 import countBy from "underscore/modules/countBy.js";
 
+const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+const mapPushEventsToRepoDetails = (events) => {
+  let pushEventsRepoDetails = [];
+  for (let i = 0; i < events.length; i++) {
+    const {
+      type,
+      repo: { id, name },
+    } = events[i];
+    if (type !== "PushEvent") break;
+    pushEventsRepoDetails.push({ id, name });
+  }
+  return pushEventsRepoDetails;
+};
+
+const getPushEventsPerRepo = (pushEventsRepoDetails) =>
+  countBy(pushEventsRepoDetails, ({ name }) => name);
+
+const mapPushEventByCountObjectToArray = (pushEventPerRepo) =>
+  Object.entries(pushEventPerRepo).map(([name, count]) => ({ name, count }));
+
+const sortByCount = (pushEventPerRepo) =>
+  pushEventPerRepo.sort((a, b) => (a.count > b.count ? -1 : 1));
+
 const Overview = compose(
   withMemo(
     "topReposCount",
-    ({ data }) => {
-      const pushEventsRepoDetails = data
-        .filter(({ type }) => type === "PushEvent")
-        .map(({ repo: { id, name } }) => ({ id, name }));
-
-      const pushEventPerRepo = countBy(
-        pushEventsRepoDetails,
-        ({ name }) => name
-      );
-
-      const sortedPushEventPerRepo = Object.entries(pushEventPerRepo)
-        .map(([name, count]) => ({ name, count }))
-        .sort((a, b) => {
-          if (a.count > b.count) return -1;
-          if (b.count > a.count) return 1;
-
-          return 0;
-        });
-      return sortedPushEventPerRepo;
-    },
+    ({ data }) =>
+      pipe(
+        mapPushEventsToRepoDetails,
+        getPushEventsPerRepo,
+        mapPushEventByCountObjectToArray,
+        sortByCount
+      )(data),
     ["data"]
   )
 )(({ topReposCount }) => (
